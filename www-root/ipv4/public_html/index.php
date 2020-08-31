87757f6a7cfdb3e7f6da3991c839f0d37ff52396
James Bensley <jwbensley@users.noreply.github.com>
2020-08-31 12:34:13 +0100

    Update index.php
    Add HTTP_X_FORWARDED_FOR for IPv4

--- www-root/ipv4/public_html/index.php
+++ www-root/ipv4/public_html/index.php
@@ -1,4 +1,4 @@
 <?php
-    echo $_SERVER['REMOTE_ADDR']; 
+    echo $_SERVER['REMOTE_ADDR'];
+    // echo $_SERVER['HTTP_X_FORWARDED_FOR']; // Use this if IPv4 is behind IPv6-to-IPv4 proxy or 6to4 NAT etc.
 ?>
-
